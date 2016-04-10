111ada5d79a17bec0a851900f6488746797a6bab
osoken <osoken.devel@outlook.jp>
2016-04-10 23:26:17 +0900

    implement config.js

--- config.js
+++ config.js
@@ -1,7 +1,50 @@
+'use strict';
 
-var config = function(_)
+var constant = function(_)
 {
+  return function(){return _;}
+}
 
+var _config = {};
+
+var config = {
+  get: function(key, _) {
+    if (_config[key] !== void 0) {
+      return _config[key].apply(this, arguments);
+    }
+    return void 0;
+  },
+  set: function(key, _) {
+    if (typeof _ === 'function') {
+      _config[key] = _;
+    }
+    else {
+      if (_ == null) {
+        delete _config[key];
+      }
+      else {
+        _config[key] = constant(_);
+      }
+    }
+    return this;
+  }
 };
 
-module.export = config;
+config.set(token, process.env.SLACK_API_TOKEN || function() {
+  throw new Error('Slack token is not set!');
+});
+
+try
+{
+  var f = require('./.config.json');
+  Object.keys(f).forEach(function(d)
+  {
+    config.set(d, f[d]);
+  });
+  config.file = '.config.json';
+}
+catch (e)
+{
+  delete config.file;
+}
+module.exports = config;
