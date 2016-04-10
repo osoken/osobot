0b1a26fb7b902e0dc1aff9988a0aaee0a8361f4b
osoken <osoken.devel@outlook.jp>
2016-04-10 23:46:39 +0900

    moved index.js's contents to osobot.js

--- osobot.js
+++ osobot.js
@@ -1,1 +1,21 @@
 'use strict';
+
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+var config = require('./config');
+
+var bot = {
+
+};
+
+var token = config.get('token');
+
+var rtm = new RtmClient(token, {logLevel: 'debug'});
+rtm.start();
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+
+});
+
+module.export = bot;
